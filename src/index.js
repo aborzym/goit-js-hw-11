6cc7073fa5fff6bc5b0cdc8e3c2a966887582713
aborzym <borzym.a@gmail.com>
2024-04-24 11:12:41 +0200

    02

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { Notify } from 'notiflix';
 axios.defaults.headers.common['x-api-key'] =
   '43544421-ff44ea561dacf5e24c1a430ca';
 
-function getPhotos(searchQuery) {
-  return axios
+const getPhotos = async (searchQuery) => {
+  await axios
     .get(
-      `https://pixabay.com/api/?key=43544421-ff44ea561dacf5e24c1a430ca&q=BMW&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=43544421-ff44ea561dacf5e24c1a430ca&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`
     )
     .then((info) => {
       return info.data;
@@ -15,7 +15,7 @@ function getPhotos(searchQuery) {
       console.log(err.message);
       console.log('no i dupa');
     });
-}
+};
 
 getPhotos('asshole')
   .then((info) => {
