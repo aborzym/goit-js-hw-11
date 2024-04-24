073df520753063c43c37e91cb47a96e467f4919a
aborzym <borzym.a@gmail.com>
2024-04-24 10:19:48 +0200

    01

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { Notify } from 'notiflix';
+axios.defaults.headers.common['x-api-key'] =
+  '43544421-ff44ea561dacf5e24c1a430ca';
+
+function getPhotos(searchQuery) {
+  return axios
+    .get(
+      `https://pixabay.com/api/?key=43544421-ff44ea561dacf5e24c1a430ca&q=BMW&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then((info) => {
+      return info.data;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      console.log('no i dupa');
+    });
+}
+
+getPhotos('asshole')
+  .then((info) => {
+    console.log(info);
+  })
+  .finally(() => console.log('all done'));
