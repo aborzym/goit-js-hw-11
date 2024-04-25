261dbf6de59149cee98e3f7b704875a885b87d13
aborzym <borzym.a@gmail.com>
2024-04-25 13:58:50 +0200

    05 - gallery working

--- src/data.js
+++ src/data.js
@@ -4,14 +4,33 @@ const apiKey = '43544421-ff44ea561dacf5e24c1a430ca';
 
 export const getPhotos = async (searchQuery) => {
   const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
-  await axios
+  return await axios
     .get(`${url}`)
     .then((info) => {
-      console.log(info.data.hits);
       return info.data.hits;
     })
     .catch((err) => {
       console.log(err.message);
       console.log('no i dupa');
     });
+};
+
+export const makeSinglePhotoHTML = (photo) => {
+  return `<div class="photo-card">
+    <img class="image" src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>${photo.likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>${photo.views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>${photo.comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>${photo.downloads}
+      </p>
+    </div>
+  </div>`;
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { getPhotos } from './data';
+import { getPhotos, makeSinglePhotoHTML } from './data';
 import axios from 'axios';
+import { error } from 'console';
 
 import { Notify } from 'notiflix';
 
@@ -8,14 +9,37 @@ const gallery = document.querySelector('div.gallery');
 const loadBtn = document.querySelector('button.load-more');
 loadBtn.classList.add('hidden');
 
-getPhotos('maserati')
-  .then((data) => {
-    console.log(data[0]);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => console.log('all done'));
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  const searchQuery = form.elements.searchQuery.value;
+
+  getPhotos(searchQuery)
+    .then((data) => {
+      if (data.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        for (let i = 0; i < 40; i++) {
+          const singlePhotoData = ({
+            largeImageURL, //link do dużego obrazka
+            webformatURL, //link do małego obrazka.
+            tags, //wiersz z opisem obrazka. Będzie pasować do atrybutu alt.
+            likes, //liczba “lajków”.
+            views, //liczba wyświetleń.
+            comments, //liczba komentarzy
+            downloads, //liczba pobrań.
+          } = data[i]);
+          console.log(largeImageURL);
+          gallery.insertAdjacentHTML(
+            'beforeend',
+            makeSinglePhotoHTML(singlePhotoData)
+          );
+        }
+      }
+    })
+    .catch();
+});
 
 //SZABLON ELEMENTU GALERII
 //   <div class="photo-card">
